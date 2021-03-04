80a2335d23bc8ff82b2366cfb5aa69b76df6bb48
ramisayeh <rami6sayeh@gmail.com>
2021-03-04 10:34:46 +0100

    v6

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import firebase from "firebase";
-import vuesax from 'vuesax';
-import 'vuesax/dist/vuesax.css';
-import vuetify from './plugins/vuetify';
+import vuesax from "vuesax";
+import "vuesax/dist/vuesax.css";
+import vuetify from "./plugins/vuetify";
 var firebaseConfig = {
   apiKey: "AIzaSyDfuayn7c9EUC0Oh1NUHzXsrYnNcnJqJWg",
   authDomain: "devchallenge-39dfc.firebaseapp.com",
@@ -12,7 +12,7 @@ var firebaseConfig = {
   storageBucket: "devchallenge-39dfc.appspot.com",
   messagingSenderId: "154951800307",
   appId: "1:154951800307:web:89b996d995ac4ccde76e86",
-  measurementId: "G-Y32YN5HEF8"
+  measurementId: "G-Y32YN5HEF8",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -23,8 +23,8 @@ new Vue({
   router,
   vuesax,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
 
-const fire = firebase;
-export default fire;
+
+export default firebase;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,29 @@
       placeholder="password..."       
       v-model="password"     
     />     
+     <div>
+                <h6 >Login With Social Media :</h6>
+                <div >
+                  <Button
+                    disabled={loading}
+                    @Click="HandleOnClick(facebookProvider)"
+                  >
+                    <FaFacebook /> Facebook
+                  </Button>
+                  <Button
+                    disabled={loading}
+                    @Click="HandleOnClick(googleProvider)"
+                  >
+                    <FaGoogle /> Google
+                  </Button>
+                  <Button
+                    disabled={loading}
+                    @Click="HandleOnClick(githubProvider)"
+                  >
+                    <FaGithub /> Github
+                  </Button>
+                </div>
+     </div>
     <button type="submit">
        Login
     </button>   
@@ -21,8 +44,16 @@
 
 <script>
 import firebase from "firebase";
-
-
+import {
+  facebookProvider,
+  githubProvider,
+  googleProvider,
+} from "./service/authmeth";
+  const HandleOnClick = async (provider) => {
+    const res = await socialMedia(provider);
+    history.push("/");
+    console.log(res);
+  };
 
 
 export default {
@@ -43,7 +74,9 @@ export default {
       .catch(error => {
         alert(error.message);
       });
-  }
+  },
+  HandleOnClick,
+      
 },
 };
 </script>
